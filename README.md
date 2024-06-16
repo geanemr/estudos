4fbe8a157ccd47ed7f1209d4cf71a203636facb0
Geane Ramos <geaneramos1995@gmail.com>
2024-06-16 11:24:05 -0300

    chore: inicio dos estudos sobre objetos

--- README.md
+++ README.md
@@ -10,4 +10,5 @@
 - [1.2. Tipos de Dados](/src//1.%20JS-para-iniciantes//1.2.%20tipos-de-dados//script.js)
 - [1.3. Números e operadores](/src/1.%20JS-para-iniciantes/1.3.%20numeros-e-operadores/script.js)
 - [1.4. Boolean e condicionais](/src/1.%20JS-para-iniciantes/1.4.%20boolean-e-condicionais/)
-- [1.5. Funções](//src/1.%20JS-para-iniciantes/1.5.%20funcoes/script.js)
+- [1.5. Funções](/src/1.%20JS-para-iniciantes/1.5.%20funcoes/script.js)
+- [1.6. Objetos](/src/1.%20JS-para-iniciantes/1.6.%20objetos/script.js)
--- src/1. JS-para-iniciantes/1.1. variaveis/script.js
+++ src/1. JS-para-iniciantes/1.1. variaveis/script.js
@@ -21,3 +21,24 @@ if (comida) {
   comida = undefined;
 }
 console.log(comida);
+
+//escopo:
+//var: global ou de função
+//let e const: de bloco{}
+
+//atualização e redeclaração:
+//var: pode ambos 
+//let: podem ser atualizadas mas não redeclaradas
+//const: não podem ser atualizadas nem declaradas novamente
+
+//hoisting
+//var: sim (undefined)
+//const e let: não são inicializadas (ReferenceError: Cannot access '' before initialization)
+
+//declaração sem inicialização(sem valor inicial)
+//var e let: sim
+//const: não ('const' declarations must be initialized)
+
+//https://www.freecodecamp.org/portuguese/news/var-let-e-const-qual-e-a-diferenca/
+
+//obs: a declaração é simplesmente informar ao compilador sobre a existência de uma variável, enquanto a inicialização é atribuir um valor inicial a essa variável
