5fd82406e1df093e748e0de8312f30c5d3850c93
Geane Ramos <geaneramos1995@gmail.com>
2024-08-16 07:34:48 -0300

    chore: classes countdown

---
+++ front-end/javascript/6. Classes/countdown/script.js
@@ -0,0 +1,5 @@
+import { Countdown } from "./countdown.js";
+
+const timeforChristmas = new Countdown('24 December 2024 23:59:59 GMT-0300')
+;
+console.log(timeforChristmas.total); //{days: 130, hours: 16, minutes: 30, seconds: 47}
