6443060b6b4f859add187aa20d9cea0f874a7fe6
geanemr <geaneramos1995@gmail.com>
2025-01-24 07:45:51 -0300

    chore: link do exercicio commonend

--- exercícios/codingJS/array/commonEnd.js
+++ exercícios/codingJS/array/commonEnd.js
@@ -7,6 +7,8 @@
 // commonEnd([1, 2, 3], [7, 3, 2]) → false
 // commonEnd([1, 2, 3], [1, 3]) → true
 
+//https://the-winter.github.io/codingjs/exercise.html?name=commonEnd&title=Array-1
+
 function commonEnd(a, b) {
   if (a[0] === b[0] || a[a.length - 1] === b[b.length - 1]) {
     return true
