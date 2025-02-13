8cf0205710bc3f2d13df810d12c28a807941c92e
geanemr <geaneramos1995@gmail.com>
2025-02-13 18:03:28 -0300

    chore: estudos typescript basico

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 ## Índice
 ### Front-end:
 1. [JavaScript](./front-end/javascript/)
+2. [TypeScript](./front-end/typescript/)
---
+++ front-end/typescript/2. typescript-basico/2.1. annotation-e-inference/script.ts
@@ -0,0 +1,93 @@
+// Annotation (anotação)
+// Com o TypeScript podemos indicar qual será o tipo de dado de uma variável através de anotações (: string).
+const produto: string = 'Livro';
+
+let preco: number = 200;
+
+const carro: {
+  marca: string;
+  portas: number;
+} = {
+  marca: 'Audi',
+  portas: 5,
+};
+
+
+// Inference (Inferência)
+// O TS consegue inferir o tipo de dado de expressões em JavaScript. Sempre que ele conseguir inferir, você não precisa fazer a anotação do dado.
+const barato = preco < 200 ? true : 'produto caro';
+
+
+// Funções
+// As anotações serão necessárias quando lidamos com funções.
+function somar(a: number, b: number) {
+    return a + b;
+  }
+  
+  somar(5, 10);
+  // somar("10", "10"); dá erro
+  
+  const nintendo = {
+    nome: 'Nintendo',
+    preco: '2000',
+  };
+  
+  function transformarPreco(produto: { nome: string; preco: string }) {
+    produto.preco = 'R$ ' + produto.preco;
+    return produto;
+  }
+  
+  const produtoNovo = transformarPreco(nintendo);
+  console.log(produtoNovo);
+
+//Exercícios
+// 1. Conserte a função com TypeScript
+// function normalizarTexto(texto) {
+//     return texto.trims().toLowercase();
+//   }
+function normalizarTexto(texto:string) {
+    return texto.trim().toLowerCase();
+  }
+
+console.log(normalizarTexto("teXTo"));
+  
+
+// 2. Conserte as funções com TypeScript
+// const input = document.querySelector('input');
+
+// const total = localStorage.getItem('total');
+// input.value = total;
+// calcularGanho(input.value);
+
+// function calcularGanho(value) {
+//   const p = document.querySelector('p');
+//   p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+// }
+
+// function totalMudou() {
+//   const value = Number(input.value);
+//   localStorage.setItem('total', value);
+//   calcularGanho(value);
+// }
+
+// input.addEventListener('keyup', totalMudou);
+const input = document.querySelector("input");
+
+const total = localStorage.getItem("total");
+if (input && total) {
+  input.value = total;
+  calcularGanho(Number(input.value));
+}
+function calcularGanho(value: number) {
+  const p = document.querySelector("p");
+  if (p) p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+}
+
+function totalMudou() {
+  if (input) {
+    localStorage.setItem("total", input.value);
+    calcularGanho(Number(input.value));
+  }
+}
+
+if (input) input.addEventListener("keyup", totalMudou);
