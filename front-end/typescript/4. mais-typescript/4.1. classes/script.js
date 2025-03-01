1d7854bdaa580d3043ca3fd243062d94bf9a7069
geanemr <geaneramos1995@gmail.com>
2025-03-01 12:06:53 -0300

    chore: classes

--- front-end/typescript/4. mais-typescript/4.1. classes/script.js
+++ front-end/typescript/4. mais-typescript/4.1. classes/script.js
@@ -38,8 +38,59 @@ class Produto6 {
         return `R$ ${preco}`;
     }
 }
+// 🔹 private: A propriedade tipo só pode ser acessada dentro da classe Produto6.
+// 🔹 protected: A propriedade preco pode ser acessada dentro da classe e por subclasses.
+// 🔹 readonly: A propriedade nome pode ser lida, mas não alterada depois de criada.
+// 🔹 static: O método transformarPreco() pertence à classe, e não a uma instância específica.
 const livro4 = new Produto6('O Senhor dos Aneis', 200);
-console.log(livro4.getTipo()); //produto
-console.log(livro4.getPreco()); //R$ 200
+// console.log(livro4.tipo); //não tem como acessar pois tipo é private, acessível só dentro da classe
+console.log(livro4.getTipo()); //produto (acessando via método)
+// console.log(livro4.preco); //não tem como acessar pois preco é protected, acessível só dentro da classe e subclasse
+console.log(livro4.getPreco()); //R$ 200 (conversão pelo método estático)
+// console.log(livro4.transformarPreço()); //não tem como acessar pois é static e só faz parte da função construtora getPreco
 console.log(livro4.nome); //O Senhor dos Aneis
 console.log(Produto6.transformarPreco(100)); //R$ 100
+//   Class e Interface
+// Ao definirmos uma classe, a sua interface é gerada automaticamente pelo TypeScript. A definição da classe é o que utilizamos para verificar se um objeto é uma instância da mesma instanceof class.
+class Quadrado {
+    lados = 4;
+    medida;
+    constructor(medida) {
+        this.medida = medida;
+    }
+    getPerimetro() {
+        return this.medida * this.lados;
+    }
+    getArea() {
+        return this.medida * this.medida;
+    }
+}
+class Circulo {
+    PI = Math.PI;
+    raio;
+    constructor(raio) {
+        this.raio = raio;
+    }
+    getPerimetro() {
+        return Math.round(2 * this.PI * this.raio * 100) / 100;
+    }
+    getArea() {
+        return Math.round(this.PI * (this.raio * this.raio) * 100) / 100;
+    }
+}
+const formas = [2, 32, 12, 3, 4, 20, 37, 9].map((n) => {
+    if (n < 15) {
+        return new Quadrado(n);
+    }
+    else {
+        return new Circulo(n);
+    }
+});
+formas.forEach((forma) => {
+    if (forma instanceof Quadrado) {
+        console.log("instance of Quadrado. Area::", forma.getArea());
+    }
+    if (forma instanceof Circulo) {
+        console.log("instance of Circulo. Perimetro:", forma.getPerimetro());
+    }
+});
