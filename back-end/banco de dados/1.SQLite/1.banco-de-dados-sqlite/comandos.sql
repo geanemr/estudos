d4d13145f05465f22e6df28f1d6a80a610d3f6b5
geanemr <geaneramos1995@gmail.com>
2026-02-11 09:05:45 -0300

    chore: inicio dos estudos sqlite

---
+++ back-end/banco de dados/1.SQLite/1.banco-de-dados-sqlite/comandos.sql
@@ -0,0 +1,7 @@
+-- database: ./db.sqlite
+
+CREATE TABLE usuarios (id, nome);
+
+INSERT INTO usuarios (id, nome) VALUES (1, 'Geane');
+
+SELECT * FROM usuarios;
---
+++ back-end/banco de dados/1.SQLite/1.banco-de-dados-sqlite/sql.js
@@ -0,0 +1,30 @@
+// Banco de Dados SQLite
+// SQL
+
+// Banco de Dados
+// DBMS (Database Management System) / SGDB
+// SQL, MongoDB, Redis
+
+// RDBMS (Relational Dabase Management System) / SGDBR
+// SQLite, MySQL, PostgreSQL
+
+// SQL
+// Structured Query Language. Linguagem de consulta estruturada.
+// https://dl.acm.org/doi/pdf/10.1145/800296.811515
+
+// CREATE TABLE usuarios (id, nome, email);
+// INSERT INTO usuarios (id, nome, email)
+// VALUES (1, 'Geane', 'geane@example.com');
+// SELECT * FROM usuarios;
+
+// SQLite
+// https://sqlite.org/
+// Não precisa configurar
+// Rápido, simples e estável
+// Instalado em mais de 1 trilhão de dispositivos
+// 90% parecido com o outros SQL
+// Outros SQL
+// Existem outras implementações do SQL. Essas implementações dependem de uma configuração/instalação inicial de um serviço/servidor que será utilizado para ser conectado com o Back End.
+// MySQL
+// MariaDB
+// PostgreSQL
